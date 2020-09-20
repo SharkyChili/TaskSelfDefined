81e3083e4ec7a3b27ba7f4f9d3a4e0a088885a94
fw1036994377@outlook.com <fw1036994377@outlook.com>
2020-09-21 02:48:56 +0800

    MppReaderTask 完成框架

--- src/main/java/com/dfjx/diy/param/reader/MppReaderParam.java
+++ src/main/java/com/dfjx/diy/param/reader/MppReaderParam.java
@@ -10,11 +10,20 @@ public class MppReaderParam extends ReaderParam {
         return "com.dfjx.diy.sync.reader.MppReaderTask";
     }
 
-    String driver = "org.postgresql.Driver";
+/*    String driver = "org.postgresql.Driver";
     //        String url = "jdbc:postgresql://localhost:5432/pgsqltest";
     String url = "jdbc:postgresql://10.1.3.40:7300/wayne";
     String username = "wayne";
-    String password = "wayne";
+    String password = "wayne";*/
+
+    public String driver;// = "org.postgresql.Driver";
+    //        String url = "jdbc:postgresql://localhost:5432/pgsqltest";
+    public String url;// = "jdbc:postgresql://10.1.3.40:7300/wayne";
+    public String username;// = "wayne";
+    public String password;// = "wayne";
+    public String sql;//"select * from wayne";
+
+
 
 
 
--- src/main/java/com/dfjx/diy/sync/reader/MppReaderTask.java
+++ src/main/java/com/dfjx/diy/sync/reader/MppReaderTask.java
@@ -1,31 +1,47 @@
 package com.dfjx.diy.sync.reader;
 
 import com.dfjx.diy.param.Param;
+import com.dfjx.diy.param.reader.MppReaderParam;
+import com.dfjx.diy.sync.Sync;
 
 import java.sql.*;
+import java.util.concurrent.BlockingQueue;
 
 public class MppReaderTask extends ReaderTask{
 
-
     @Override
-    Object produce() {
-        return null;
-    }
+    public void init(Param param, BlockingQueue<Object> queue, Sync sync) {
+        super.init(param, queue, sync);
 
-    @Override
-    Object[] produceBatch() {
-        //不用
-        return new Object[0];
-    }
+        MppReaderParam mppReaderParam = (MppReaderParam) param;
+/*        String driver = "org.postgresql.Driver";
+//        String url = "jdbc:postgresql://localhost:5432/pgsqltest";
+        String url = "jdbc:postgresql://10.1.3.40:7300/wayne";
+        String username = "wayne";
+        String password = "wayne";*/
+        String driver = mppReaderParam.driver;
+        String url = mppReaderParam.url;
+        String username = mppReaderParam.username;
+        String password = mppReaderParam.password;
 
-    public void run() {
-        Connection conn = getConn();
-        String sql = "select * from wayne";
+        Connection conn = null;
+        try {
+            Class.forName(driver); // classLoader,加载对应驱动
+            conn = (Connection) DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String sql = ((MppReaderParam)this.param).sql;
         PreparedStatement pstmt;
         try {
             pstmt = (PreparedStatement) conn.prepareStatement(sql);
             ResultSet rs = pstmt.executeQuery();
-            int col = rs.getMetaData().getColumnCount();
+
+            //todo 放到内存里吧还是，以后再改
+            /*int col = rs.getMetaData().getColumnCount();
             System.out.println("============================");
             while (rs.next()) {
                 for (int i = 1; i <= col; i++) {
@@ -36,39 +52,22 @@ public class MppReaderTask extends ReaderTask{
                 }
                 System.out.println("");
             }
-            System.out.println("============================");
+            System.out.println("============================");*/
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+    @Override
+    Object produce() {
+        //todo
+        return null;
+    }
 
-    /**
-     * @return Connection
-     * @method getConn() 获取数据库的连接
-     */
-    public Connection getConn() {
-        /*<dependency>
-            <groupId>postgresql</groupId>
-            <artifactId>postgresql</artifactId>
-            <version>9.1-901-1.jdbc4</version>
-        </dependency>*/
-        String driver = "org.postgresql.Driver";
-//        String url = "jdbc:postgresql://localhost:5432/pgsqltest";
-        String url = "jdbc:postgresql://10.1.3.40:7300/wayne";
-        String username = "wayne";
-        String password = "wayne";
-        Connection conn = null;
-        try {
-            Class.forName(driver); // classLoader,加载对应驱动
-            conn = (Connection) DriverManager.getConnection(url, username, password);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return conn;
-
+    @Override
+    Object[] produceBatch() {
+        //不用
+        return new Object[0];
     }
 
 }
