20df6cc770c9e6199739363e9e4beb4c9a60469b
fw1036994377@outlook.com <fw1036994377@outlook.com>
2020-09-21 23:09:58 +0800

    加入自定义相关内容

--- src/main/java/com/dfjx/diy/sync/writer/HdfsWriterTask.java
+++ src/main/java/com/dfjx/diy/sync/writer/HdfsWriterTask.java
@@ -12,4 +12,20 @@ public class HdfsWriterTask extends WriterTask {
     void consumeBatch(Object[] objects) {
 
     }
+
+
+/*
+    conf.addResource(new Path("/etc/hadoop/conf/core-site.xml"));
+conf.addResource(new Path("/etc/hadoop/conf/hdfs-site.xml"));
+
+//参数 tbds_username，tbds_username，tbds_securekey通过工作流自定义参数传入
+
+conf.set("hadoop.security.authentication", "tbds");
+conf.set("hadoop_security_authentication_tbds_username",tbds_username);
+conf.set("hadoop_security_authentication_tbds_secureid",tbds_secureid);
+conf.set("hadoop_security_authentication_tbds_securekey",tbds_securekey);
+UserGroupInformation.setConfiguration( conf );
+UserGroupInformation.loginUserFromSubject(null);
+*/
+
 }
---
+++ src/main/java/com/tencent/taskrunner/FieldsModel.java
@@ -0,0 +1,16 @@
+package com.tencent.taskrunner;
+
+public class FieldsModel {
+
+	private String fields;
+
+	public String getFields() {
+		return fields;
+	}
+
+	public void setFields(String fields) {
+		this.fields = fields;
+	}
+
+	
+}
